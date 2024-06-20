c25604f8a18489cde607ccc88fca20454809ec21
KsHuyZ <92804746+KsHuyZ@users.noreply.github.com>
2024-06-20 10:15:24 +0700

    commit

--- contracts/Certificate.sol
+++ contracts/Certificate.sol
@@ -9,6 +9,7 @@ contract CertificateContract {
         string courseId;
         string completedPoint;
         uint256 timestamp;
+        bool isCompleted;
     }
     uint256 taxPrice = 0.0025 ether;
     mapping(string => mapping(string => Certificate)) certificates;
@@ -17,26 +18,53 @@ contract CertificateContract {
         string userId,
         string courseId,
         string completedPoint,
-        uint256 timestamp
+        uint256 timestamp,
+        bool isCompleted
     );
+
+    function createCertificate(
+        string memory _userId,
+        string memory _courseId
+    ) public payable {
+        require(msg.value == taxPrice, "Price must be equal to listing price");
+        Certificate memory newCertificate = Certificate({
+            userId: _userId,
+            courseId: _courseId,
+            completedPoint: '0',
+            timestamp: 0,
+            isCompleted: false
+        });
+        certificates[_userId][_courseId] = newCertificate;
+        emit CertificateAdded(
+            _userId,
+            _courseId,
+            '0',
+            0,
+            false
+        );
+    }
+
     function addCertificate(
         string memory _userId,
         string memory _courseId,
         string memory _completedPoint
     ) external {
-        require(
-            bytes(certificates[_userId][_courseId].userId).length == 0,
-            "Certificate already exists"
-        );
         uint256 _timestamp = block.timestamp;
-        Certificate memory newCertificate = Certificate({
+        Certificate memory updateCertificate = Certificate({
             userId: _userId,
             courseId: _courseId,
             completedPoint: _completedPoint,
-            timestamp: _timestamp
+            timestamp: _timestamp,
+            isCompleted: true
         });
-        certificates[_userId][_courseId] = newCertificate;
-        emit CertificateAdded(_userId, _courseId, _completedPoint, _timestamp);
+        certificates[_userId][_courseId] = updateCertificate;
+        emit CertificateAdded(
+            _userId,
+            _courseId,
+            _completedPoint,
+            _timestamp,
+            true
+        );
     }
 
     function getCertificate(
